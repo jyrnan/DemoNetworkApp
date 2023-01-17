c86f0247aa572e15ef8f552f9b3696a0a7102031
jyrnan <yoeking@gmail.com>
2023-01-17 09:35:46 +0800

    Initial Commit

---
+++ DemoNetworkApp/DemoNetworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoNetworkAppApp.swift
+//  DemoNetworkApp
+//
+//  Created by jyrnan on 2023/1/17.
+//
+
+import SwiftUI
+
+@main
+struct DemoNetworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
